5072a77492a35d3f536274f4ad8ed8b42e45a5dd
Pranav-Rawat <pranavrawat2005@gmail.com>
2026-08-29 16:35:32 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -15,9 +15,9 @@ module FA_Gate(
   output cout
 );
   wire ps, pc1, pc2;
-  and (pc1, a,   b);
-  xor (ps,  a,   b);
-  and (pc2, cin, ps);
+ xor (ps,  a,   b);
+ and (pc1, a,   b);
   xor (sum, cin, ps);
+  and (pc2, cin, ps);
   or  (cout, pc1, pc2);
 endmodule
